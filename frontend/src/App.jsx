afe0ddfaebb77f322406bc33b0c6ac4207b3adfa
Krushna <fullstack.kanha@gmail.com>
2024-09-01 19:59:08 +0530

    1- Project Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+
+const App = () => {
+  return (
+    <div className="">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
