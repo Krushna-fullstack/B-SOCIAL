c8855001978fd564ca1aaa29a041772b1c8f163a
Krushna <fullstack.kanha@gmail.com>
2024-09-02 20:02:09 +0530

    1-  add getMe controller for retrieving user information

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import {
+  signup,
+  login,
+  logout,
+  getMe,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middlewares/protectRoute.js";
 
 const router = Router();
 
+router.get("/me", protectRoute, getMe);
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
