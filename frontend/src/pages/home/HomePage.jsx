795370ce97b6f59e318e3aae04d027160641f52a
Subhransu Sekhar Mishra <msubhransu1@gmail.com>
2024-09-04 19:51:44 +0530

    2 - Navbar UI Created

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,9 +1,76 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div>
-    <h1>Homepage</h1>
-  </div>;
+  return (
+    <div>
+      <h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        consequatur, quasi vitae obcaecati natus perferendis. Rerum consequuntur
+        nemo quas optio repellat deserunt eos modi expedita suscipit autem,
+        dolorum animi cum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        consequatur, quasi vitae obcaecati natus perferendis. Rerum consequuntur
+        nemo quas optio repellat deserunt eos modi expedita suscipit autem,
+        dolorum animi cum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        consequatur, quasi vitae obcaecati natus perferendis. Rerum consequuntur
+        nemo quas optio repellat deserunt eos modi expedita suscipit autem,
+        dolorum animi cum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        consequatur, quasi vitae obcaecati natus perferendis. Rerum consequuntur
+        nemo quas optio repellat deserunt eos modi expedita suscipit autem,
+        dolorum animi cum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        consequatur, quasi vitae obcaecati natus perferendis. Rerum consequuntur
+        nemo quas optio repellat deserunt eos modi expedita suscipit autem,
+        dolorum animi cum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        consequatur, quasi vitae obcaecati natus perferendis. Rerum consequuntur
+        nemo quas optio repellat deserunt eos modi expedita suscipit autem,
+        dolorum animi cum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        consequatur, quasi vitae obcaecati natus perferendis. Rerum consequuntur
+        nemo quas optio repellat deserunt eos modi expedita suscipit autem,
+        dolorum animi cum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        consequatur, quasi vitae obcaecati natus perferendis. Rerum consequuntur
+        nemo quas optio repellat deserunt eos modi expedita suscipit autem,
+        dolorum animi cum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        consequatur, quasi vitae obcaecati natus perferendis. Rerum consequuntur
+        nemo quas optio repellat deserunt eos modi expedita suscipit autem,
+        dolorum animi cum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        consequatur, quasi vitae obcaecati natus perferendis. Rerum consequuntur
+        nemo quas optio repellat deserunt eos modi expedita suscipit autem,
+        dolorum animi cum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        consequatur, quasi vitae obcaecati natus perferendis. Rerum consequuntur
+        nemo quas optio repellat deserunt eos modi expedita suscipit autem,
+        dolorum animi cum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        consequatur, quasi vitae obcaecati natus perferendis. Rerum consequuntur
+        nemo quas optio repellat deserunt eos modi expedita suscipit autem,
+        dolorum animi cum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        consequatur, quasi vitae obcaecati natus perferendis. Rerum consequuntur
+        nemo quas optio repellat deserunt eos modi expedita suscipit autem,
+        dolorum animi cum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        consequatur, quasi vitae obcaecati natus perferendis. Rerum consequuntur
+        nemo quas optio repellat deserunt eos modi expedita suscipit autem,
+        dolorum animi cum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        consequatur, quasi vitae obcaecati natus perferendis. Rerum consequuntur
+        nemo quas optio repellat deserunt eos modi expedita suscipit autem,
+        dolorum animi cum?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+        consequatur, quasi vitae obcaecati natus perferendis. Rerum consequuntur
+        nemo quas optio repellat deserunt eos modi expedita suscipit autem,
+        dolorum animi cum?
+      </h1>
+    </div>
+  );
 };
 
 export default HomePage;
