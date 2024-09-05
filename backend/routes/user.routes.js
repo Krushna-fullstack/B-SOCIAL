fb91b62ceb5599ded7138b71609cf1eb1dbb4bc4
Krushna <fullstack.kanha@gmail.com>
2024-09-06 00:39:01 +0530

    3 - add follow/unfollow functionality

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
 import { protectRoute } from "../middlewares/protectRoute.js";
-import { getUserProfile, updateUser } from "../controllers/user.controller.js";
+import {
+  getUserProfile,
+  updateUser,
+  followUnfollowUser,
+} from "../controllers/user.controller.js";
 
 const router = Router();
 
 router.get("/profile/:username", protectRoute, getUserProfile);
+router.post("/follow/:id", protectRoute, followUnfollowUser);
 router.put("/update", protectRoute, updateUser);
 
 export default router;
