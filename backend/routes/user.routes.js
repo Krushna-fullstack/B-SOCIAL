cd74f5df42d89945a5fa63d2e00c04df11bbf89c
Krushna <fullstack.kanha@gmail.com>
2024-09-06 00:06:15 +0530

    3 - Add updateUser route and controller

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import { protectRoute } from "../middlewares/protectRoute.js";
+import { getUserProfile, updateUser } from "../controllers/user.controller.js";
 
 const router = Router();
 
-router.get("/profile/:username", getUserProfile);
+router.get("/profile/:username", protectRoute, getUserProfile);
+router.put("/update", protectRoute, updateUser);
 
 export default router;
