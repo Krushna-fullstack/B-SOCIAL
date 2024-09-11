24d7551d0d7f933d94d2a2a5be53f0fa7de5db3d
Krushna <fullstack.kanha@gmail.com>
2024-09-11 19:33:18 +0530

    5 -  deletePost and commentOnPost routes and controllers

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -39,3 +39,63 @@ export const createPost = asyncHandler(async (req, res) => {
     console.log("Error in createPost controller: ", error);
   }
 });
+
+export const deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    if (post.user.toString() !== req.user._id.toString()) {
+      return res
+        .status(401)
+        .json({ error: "You are not authorized to delete this post" });
+    }
+
+    if (post.img) {
+      const imgId = post.img.split("/").pop().split(".")[0];
+      await clodinary.uploader.destroy(imgId);
+    }
+
+    await Post.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ message: "Post deleted successfully" });
+  } catch (error) {
+    console.log("Error in deletePost controller: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const commentOnPost = asyncHandler(async (req, res) => {
+  try {
+    const { text } = req.body;
+    const postId = req.params.id;
+    const userId = req.user._id;
+
+    if (!text) {
+      return res.status(400).json({ error: "Text is required" });
+    }
+
+    const post = await Post.findById(postId);
+
+    if (!post) {
+      return res.status(404).json({ error: "Post not found" });
+    }
+
+    const comment = {
+      user: userId,
+      text,
+    };
+
+    post.comments.push(comment);
+
+    await post.save();
+
+    res.status(201).json(post);
+  } catch (error) {
+    console.log("Error in commentOnPost controller: ", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import { protectRoute } from "../middlewares/protectRoute.js";
+import {
+  commentOnPost,
+  createPost,
+  deletePost,
+} from "../controllers/post.controller.js";
 
 const router = Router();
 
 router.post("/create", protectRoute, createPost);
+router.delete("/:id", protectRoute, deletePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
 
 export default router;
