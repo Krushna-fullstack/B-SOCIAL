00c125cd1a1317cd469a5ab11320317e5c16b2ea
Krushna <fullstack.kanha@gmail.com>
2024-09-11 19:48:04 +0530

    7 - Add getAllPosts route and controller for retrieving all posts

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -4,11 +4,13 @@ import {
   commentOnPost,
   createPost,
   deletePost,
+  getAllPosts,
   likeUnlikePost,
 } from "../controllers/post.controller.js";
 
 const router = Router();
 
+router.get("/all", protectRoute, getAllPosts);
 router.post("/create", protectRoute, createPost);
 router.delete("/:id", protectRoute, deletePost);
 router.post("/comment/:id", protectRoute, commentOnPost);
