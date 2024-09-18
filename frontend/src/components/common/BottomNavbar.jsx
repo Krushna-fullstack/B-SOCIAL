cb978d5ee2e7d7bd59703691020d97c03846ed48
Krushna <fullstack.kanha@gmail.com>
2024-09-18 18:56:02 +0530

    Refactor PostSkeleton and BottomNavbar components

--- frontend/src/components/common/BottomNavbar.jsx
+++ frontend/src/components/common/BottomNavbar.jsx
@@ -1,39 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { HiOutlineUserGroup } from "react-icons/hi2";
 import { MdContentCopy } from "react-icons/md";
-import { AiOutlineFilePdf } from "react-icons/ai";
+import { AiOutlineFilePdf, AiOutlineHome } from "react-icons/ai";
 import { RiHotelLine } from "react-icons/ri";
 import { PiHandbag } from "react-icons/pi";
 
-function BottomNavbar() {
+const BottomNavbar = () => {
   const navItems = [
-    { icon: HiOutlineUserGroup, label: "Community", to: "" },
-    { icon: MdContentCopy, label: "Notice", to: "" },
-    { icon: AiOutlineFilePdf, label: "Resource", to: "" },
-    { icon: RiHotelLine, label: "PG", to: "" },
+    { icon: AiOutlineHome, label: "Home", to: "/" },
+    { icon: MdContentCopy, label: "Notice", to: "/notice" },
+    { icon: AiOutlineFilePdf, label: "Resource", to: "/resources" },
+    { icon: RiHotelLine, label: "PG", to: "/pg" },
     { icon: PiHandbag, label: "Job", to: "/jobs" },
   ];
 
   return (
-    <div
-      className="fixed inset-x-0 bottom-0 lg:left-0 lg:top-0 lg:w-[20%] lg:h-full bg-base-100 z-10 p-2 lg:p-4"
-      style={{ borderRight: "1px solid gray" }}
-    >
-      <div className="flex justify-evenly lg:flex-col lg:items-center lg:justify-around lg:h-full lg:w-38">
+    <div className="fixed bottom-0 left-0 w-full lg:w-[20%] lg:h-full bg-base-100 z-10 p-4 border-t lg:border-t-0 lg:border-r border-gray-300">
+      <div className="flex justify-between lg:flex-col lg:justify-evenly lg:items-center lg:h-full">
         {navItems.map(({ icon: Icon, label, to }) => (
           <Link
             key={label}
-            className="btm-nav-icon text-neutral-content hover:text-primary flex flex-col items-center lg:space-x-2"
+            className="flex flex-col items-center justify-center space-y-1 lg:space-x-0 text-neutral-content hover:text-primary"
             to={to}
           >
-            <Icon className="w-9 h-8" />
-            <span className="text-xs lg:text-lg font-semibold">{label}</span>
+            <Icon className="w-8 h-8 lg:w-10 lg:h-10" />
+            <span className="text-xs lg:text-sm font-medium">{label}</span>
           </Link>
         ))}
       </div>
     </div>
   );
-}
+};
 
 export default BottomNavbar;
