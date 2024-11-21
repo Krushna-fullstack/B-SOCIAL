55627adbe79ffbf7e609f7e9b30ba21d04a27c84
Krushna <fullstack.kanha@gmail.com>
2024-11-21 22:39:36 +0530

    Update server.js to listen on all network interfaces and change API proxy target in vite.config.js

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const HOST = "0.0.0.0";
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
@@ -55,7 +56,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDB();
 });
