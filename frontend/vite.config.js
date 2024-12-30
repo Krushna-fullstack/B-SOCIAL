0a50d46248528930367bfe0384efaf0d92ac1393
Subhransu Sekhar Mishra <msubhransu1@gmail.com>
2024-12-30 15:35:49 +0530

    Target path URL Edit - vite.config.js file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,8 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
-        // target: "http://64.227.132.195:5000",
+        // target: "http://localhost:5000",
+        target: "http://64.227.132.195:5000",
         changeOrigin: true,
       },
     },
