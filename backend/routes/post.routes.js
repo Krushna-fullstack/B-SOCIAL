82e54656f8e9f4c09cb35483bdc295d32973e1b2
Krushna <fullstack.kanha@gmail.com>
2025-09-01 21:09:28 +0530

    Remove protectRoute middleware from the getAllPosts route for improved access.

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -12,7 +12,7 @@ import {
 
 const router = Router();
 
-router.get("/all", protectRoute, getAllPosts);
+router.get("/all", getAllPosts);
 router.get("/user/:username", protectRoute, getUserPosts);
 router.post("/create", protectRoute, createPost);
 router.delete("/:id", protectRoute, deletePost);
